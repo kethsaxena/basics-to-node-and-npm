b3a4f69872fd7bef401b593eea968dea17735949
Praketa <praketasaxena@gmail.com>
2020-01-21 18:37:18 -0600

    -test code

---
+++ index.js
@@ -0,0 +1,11 @@
+
+console.log('Hello, world!');
+console.log(process.argv);
+
+let foo ='abc';
+console.log(foo);
+
+const fs = require('fs');
+const contents = fs.readdirSync('./');
+console.log(contents);
+
